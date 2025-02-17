02efde3fbf4ee9302f36a7069a9b45eb2ab88a5e
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-02-17 10:36:27 +0100

    Adding submenu and music

--- menu.py
+++ menu.py
@@ -40,24 +40,27 @@ class Button:
                 pygame.mixer.Sound.play(sound)
 
 class Menu:
-    def __init__(self, screen):
+    def __init__(self, screen, switch_to_submenu):
         self.screen = screen
         self.font = pygame.font.Font(None, 40)
         self.background = pygame.image.load("assets/images/menu_background.jpg")
         self.buttons = [
-            Button(150, 100, 200, 50, "Start", self.font, (255, 255, 255), (255, 255, 255), self.start_game),
-            Button(150, 200, 200, 50, "Options", self.font, (255, 255, 255), (255, 255, 255), self.show_options),
-            Button(150, 300, 200, 50, "Quit", self.font, (255, 255, 255), (255, 255, 255), pygame.quit)
+            Button(200, 150, 400, 50, "Start", self.font, (255, 255, 255), (255, 255, 255), self.start_game),
+            Button(200, 250, 400, 50, "Pokedex", self.font, (255, 255, 255), (255, 255, 255), self.pokedex),
+            Button(200, 350, 400, 50, "Quit", self.font, (255, 255, 255), (255, 255, 255), pygame.quit),
+            Button(200, 450, 400, 50, "Go to SubMenu", self.font, (255, 255, 255), (255, 255, 255), switch_to_submenu)  # Test button
         ]
     
     def start_game(self):
         print("Starting game...")
     
-    def show_options(self):
-        print("Showing options...")
+    def pokedex(self):
+        print("Pokedex...")
     
     def draw(self):
-        self.screen.blit(self.background, (0, 0))
+        # Resize background to fit the screen
+        background = pygame.transform.scale(self.background, self.screen.get_size())
+        self.screen.blit(background, (0, 0))
         for button in self.buttons:
             button.draw(self.screen)
     
@@ -65,23 +68,67 @@ class Menu:
         for button in self.buttons:
             button.handle_event(event)
 
-if __name__ == "__main__":
+class SubMenu:
+    def __init__(self, screen, switch_to_main_menu):
+        self.screen = screen
+        self.font = pygame.font.Font(None, 40)
+        self.background = pygame.image.load("assets/images/submenu_background.jpg")  # Different background image
+        self.buttons = [
+            Button(200, 200, 400, 50, "Resume", self.font, (255, 255, 255), (255, 255, 255), self.resume_game),
+            Button(200, 300, 400, 50, "Main Menu", self.font, (255, 255, 255), (255, 255, 255), switch_to_main_menu)
+        ]
+    
+    def draw(self):
+        # Resize background to fit the screen
+        background = pygame.transform.scale(self.background, self.screen.get_size())
+        self.screen.blit(background, (0, 0))
+        for button in self.buttons:
+            button.draw(self.screen)
+    
+    def handle_event(self, event):
+        for button in self.buttons:
+            button.handle_event(event)
+
+    def resume_game(self):
+        print("Resuming game...")
+
+def main():
     pygame.init()
-    screen = pygame.display.set_mode((500, 500))
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    
+    def switch_to_submenu():
+        nonlocal current_menu, sound
+        pygame.mixer.Sound.stop(sound)
+        sound = pygame.mixer.Sound("assets/sounds/Mediator_menu.mp3")
+        pygame.mixer.Sound.play(sound, -1)
+        current_menu = submenu
+    
+    def switch_to_main_menu():
+        nonlocal current_menu, sound
+        pygame.mixer.Sound.stop(sound)
+        sound = pygame.mixer.Sound("assets/sounds/Main_menu.mp3")
+        pygame.mixer.Sound.play(sound, -1)
+        current_menu = menu
+    
+    menu = Menu(screen, switch_to_submenu)
+    submenu = SubMenu(screen, switch_to_main_menu)
+    current_menu = menu
     sound = pygame.mixer.Sound("assets/sounds/Main_menu.mp3")
     pygame.mixer.Sound.play(sound, -1)
-    clock = pygame.time.Clock()
-    menu = Menu(screen)
     
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            menu.handle_event(event)
+            current_menu.handle_event(event)
         
-        menu.draw()
+        current_menu.draw()
         pygame.display.flip()
         clock.tick(30)
     
     pygame.quit()
+
+if __name__ == "__main__":
+    main()
